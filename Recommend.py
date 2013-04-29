a0e02e633266000f81809ca75eb610556da1733a
mnguyen5904 <mnguyen5904@11aaf2a9-4230-98c4-d350-ce57b7db7bff>
2013-04-29 20:25:30 +0000

    Update Recommend.py
    git-svn-id: https://github.com/Inconditus/win.git/trunk@44 11aaf2a9-4230-98c4-d350-ce57b7db7bff

--- Recommend.py
+++ Recommend.py
@@ -1,2 +1,30 @@
 """Recommender system processing module
 """
+
+import DataAccess
+import RecommendOutput
+
+
+def recommend(user):
+  """Recommend wines to user
+  """
+
+
+def getWinesByAttr(attributes):
+  """Retrieve wines with qualitiessimilar to attributes
+  """
+  
+  
+def getWinesByName(name):
+  """Retrieve wines with the given name
+  """
+  
+  
+def addWineGlobal(wine)
+  """Add new wine to the global database
+  """
+  
+  
+def randomWines():
+  """Retrieve a random set of wines
+  """
