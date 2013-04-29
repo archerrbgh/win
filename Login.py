982ccde677b55a933a405469f9604bd00e909ec8
mnguyen5904 <mnguyen5904@11aaf2a9-4230-98c4-d350-ce57b7db7bff>
2013-04-29 19:24:22 +0000

    Update Login.py
    git-svn-id: https://github.com/Inconditus/win.git/trunk@34 11aaf2a9-4230-98c4-d350-ce57b7db7bff

--- Login.py
+++ Login.py
@@ -1,6 +1,12 @@
 """User login preprocessing module
 """
 
+
+def inputCreateUser(username, password):
+  """Preprocess new user info
+  """
+
+
 def inputLogin(username, password):
   """Preprocess login information
   """
