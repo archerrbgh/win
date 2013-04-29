bcccbc7b4be70e97fe4be6f891b9a482ccc89141
mnguyen5904 <mnguyen5904@11aaf2a9-4230-98c4-d350-ce57b7db7bff>
2013-04-29 20:22:06 +0000

    Update RecommendInput.py
    git-svn-id: https://github.com/Inconditus/win.git/trunk@42 11aaf2a9-4230-98c4-d350-ce57b7db7bff

--- RecommendInput.py
+++ RecommendInput.py
@@ -1,2 +1,25 @@
 """Recommender system preprocessing module
 """
+
+import Recommend
+
+
+def inputGetWinesByAttr(attributes):
+  """Preprocess retrieval of wines with qualities 
+  similar to attributes
+  """
+  
+  
+def inputGetWinesByName(name):
+  """Preprocess retrieval of wines with the given name
+  """
+  
+  
+def inputAddWineGlobal(wine)
+  """Preprocess addition of new wine to the global database
+  """
+  
+  
+def inputRandomWines():
+  """Preprocess retrieval of a random set of wines
+  """
