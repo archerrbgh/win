7cd31e6b658e009bc59dc89202891c060ed05ce2
mnguyen5904 <mnguyen5904@11aaf2a9-4230-98c4-d350-ce57b7db7bff>
2013-04-29 19:56:05 +0000

    Update InventoryInput.py
    git-svn-id: https://github.com/Inconditus/win.git/trunk@36 11aaf2a9-4230-98c4-d350-ce57b7db7bff

--- InventoryInput.py
+++ InventoryInput.py
@@ -1,2 +1,34 @@
 """Inventory system preprocessing module
 """
+
+
+def inputGetInventory(user):
+  """Preprocess retrieval of user's inventory
+  """
+  
+  
+def inputAddUserWine(user, wine):
+  """Preprocess addition of wine to user's inventory
+  """
+  
+
+def inputAddUserStorage(user, storage):
+  """Preprocess addition of storage location to
+  user's inventory
+  """
+  
+  
+def inputDeleteUserWine(user, wine):
+  """Preprocess deletion of wine from user's inventory
+  """
+  
+  
+def inputDeleteUserStorage(user, storage):
+  """Preprocess deletion of user's storage location
+  """
+  
+  
+def inputAddToStorage(user, storage, wine):
+  """Preprocess addition of wine to user's storage
+  location
+  """
