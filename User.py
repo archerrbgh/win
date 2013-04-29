a306d165352dc5f2360b31d38f3c842fc822bca8
mnguyen5904 <mnguyen5904@11aaf2a9-4230-98c4-d350-ce57b7db7bff>
2013-04-29 19:23:29 +0000

    Update User.py
    git-svn-id: https://github.com/Inconditus/win.git/trunk@33 11aaf2a9-4230-98c4-d350-ce57b7db7bff

--- User.py
+++ User.py
@@ -1,2 +1,17 @@
 """User account module
 """
+
+
+def createUser(username, password):
+  """Process new user info before adding to database
+  """
+
+
+def login(username, password):
+  """Login user if information is valid
+  """
+  
+
+def logout():
+  """Logout user
+  """
