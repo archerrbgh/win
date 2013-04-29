68632bfe557f3589fa2dbe268aeb91e78c61d163
mnguyen5904 <mnguyen5904@11aaf2a9-4230-98c4-d350-ce57b7db7bff>
2013-04-29 20:00:08 +0000

    Update Inventory.py
    git-svn-id: https://github.com/Inconditus/win.git/trunk@37 11aaf2a9-4230-98c4-d350-ce57b7db7bff

--- Inventory.py
+++ Inventory.py
@@ -1,2 +1,35 @@
 """Inventory system processing module
 """
+
+import DataAccess
+import InventoryOutput
+
+
+def getInventory(user):
+  """Retrieve user's inventory
+  """
+  
+  
+def addUserWine(user, wine):
+  """Add wine to user's inventory
+  """
+  
+
+def addUserStorage(user, storage):
+  """Add storage location to user's inventory
+  """
+  
+  
+def deleteUserWine(user, wine):
+  """Delete wine from user's inventory
+  """
+  
+  
+def deleteUserStorage(user, storage):
+  """Delete user's storage location
+  """
+  
+  
+def addToStorage(user, storage, wine):
+  """Addition wine to user's storagelocation
+  """
