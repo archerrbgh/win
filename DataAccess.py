e82bff0ec161ce27202bf75724362997af6e586c
mnguyen5904 <mnguyen5904@11aaf2a9-4230-98c4-d350-ce57b7db7bff>
2013-05-05 16:36:29 +0000

    Update DataAccess.py
    git-svn-id: https://github.com/Inconditus/win.git/trunk@77 11aaf2a9-4230-98c4-d350-ce57b7db7bff

--- DataAccess.py
+++ DataAccess.py
@@ -12,9 +12,8 @@ def dbGetUser(userID):
     int userID: The userID of the user being retrieved.
     
   Return:
-    dict: A dictionary specifying relevant information about the user, in the form
-          {name:*, email:*, etc}. Store enough information to generate a user
-          profile page.
+    User: A User object specifying relevant information about the user}. Store enough 
+          information to generate a user profile page.
   
   """
 
@@ -33,14 +32,14 @@ def dbCreateUser(username, password, attr = 0):
   """
   
 
-def dbUpdateUser(userID, attr):
+def dbUpdateUser(user, attr):
   """Update user profile in database with attributes
   
   Change information stored about the user in the database to match relevant information
   passed in attr.
   
   Args:
-    int userID: The userID of the user being modified.
+    Object user: The User object of the user being modified.
     dict attr: A dict specifying user attributes, of the form {email:*, DOB:*, etc}. Not every
                field stored in the database has to be present.
   """
@@ -81,59 +80,77 @@ def dbRandomWines():
   
 # INVENTORY SYSTEM
 
-def dbGetInventory(userID):
+def dbGetInventory(user):
   """Retrieve a user's inventory from database
   
   Return a list of wineIDs representing the wines in a user's inventory.
   
   Args:
-    int userID: The userID of the user.
+    User user: The User object of the user.
     
   Return:
-    list: A list of wineIDs. Contains the wineID of every wine in the user's
-          inventory.
+    list: A list of Location objects detailing the user's entire inventory. 
   """
   
   
-def dbAddWineUser(userID, wine, count, locationID = -1):
+def dbAddWineUser(user, wine, count, location = -1):
   """Add a wine to the user's inventory in database
   
   Insert the given wine into the specified inventory.
   Include the attributes in wine, which will include a new
-  wineID. If locationID is unspecified, store the wine in the user's
+  wineID. If location is unspecified, store the wine in the user's
   global inventory.
   
   Args:
-    int userID: The userID of the user.
-    dict wine: A dict specifying wine attributes, of the form 
-              {wineID:*, wineName:*, etc}.
+    User user: The User object of the user.
+    Wine wine: A Wine object representing a wine.
     int count: The number of this wine to add.
-    int locationID: If specified, the locationID of the inventory to
-                    insert the wine into.
+    Location location: If specified, the location of the inventory to
+                       insert the wine into.
   """
   
   
-def dbAddInventory(user, storage):
-  """Add a new storage location to user's profile
-  in database
+def dbAddInventory(user, location):
+  """Add a new inventory location to user's profile in database
+  
+  Insert a new inventory location into the user's Location Map.
+  
+  Args:
+    User user: The User object of the user.
+    Location location: The Location object of the location.
   """
   
   
-def dbDeleteWineUser(user, wine):
+def dbDeleteWineUser(user, wine, count):
   """Delete a wine from user's inventory in database
+  
+  Delete count amounts of the wine from the user's locations.
+  
+  Args:
+    User user: The User object of the user.
+    Wine wine: The Wine object of the wine.
   """
   
   
-def dbDeleteStorage(user, storage):
-  """Delete a user's storage location in database
-  """
+def dbDeleteInventory(user, location):
+  """Delete a user's inventory location in database
   
+  Delete an inventory location from user's Location Map.
   
-def dbAddToStorage(user, storage, wine):
-  """Add wine to a user's storage location in database
+  Args:
+    User user: The User object of the user.
+    Location location: The Location object of the location.
   """
   
   
 def dbRateWineUser(user, wine, rating):
   """Store user's rating of wine in database
+  
+  Store the rating of the specified wine in the user's inventory.
+  
+  Args:
+    User user: The User object of the user.
+    Wine wine: The wine object of the wine.
+    int rating: The rating on a 1-5 scale.
+  
   """
