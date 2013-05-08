ab179fd39b80107eec1f0dd7ae6cdd1ab3019a2f
mnguyen5904 <mnguyen5904@11aaf2a9-4230-98c4-d350-ce57b7db7bff>
2013-05-08 09:08:01 +0000

    Update User.py
    git-svn-id: https://github.com/Inconditus/win.git/trunk@100 11aaf2a9-4230-98c4-d350-ce57b7db7bff

--- User.py
+++ User.py
@@ -1,20 +1,49 @@
 """User account module
 """
 
-import DataAccess
-import ProfileOutput
 
-
-def createUser(username, password):
+def createUser(user):
   """Process new user info before adding to database
+  
+  Check if username and password aren't already taken. If not, create
+  a new user.
+  
+  Args:
+    dict user: Dict representing the UserInfo object of the potential new user
+    
+  Return:
+    UserInfo: The newly created user object
   """
 
 
 def login(username, password):
   """Login user if information is valid
+  
+  Args:
+    string username: The potential username
+    string password: The potential password
+    
+  Return:
+    UserInfo: UserInfo object of the logged in user
   """
   
 
 def logout():
   """Logout user
+  
+  Args:
+    UserInfo user: UserInfo object of the user being logged out.
+  """
+  
+
+def editUser(user):
+  """Call DataAccess layer to update user profile in database with attributes
+  
+  Change information stored about the user in the database to match relevant information
+  passed in attr.
+  
+  Args:
+    UserInfo user: The UserInfo object of the user being modified.
+    dict attr: A dict specifying user attributes, of the form {email:*, DOB:*, etc}. Not every
+               field stored in the database has to be present.
   """
