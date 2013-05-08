54689d877be8f29fe7cd74ea93eca29e3b06e8d3
mnguyen5904 <mnguyen5904@11aaf2a9-4230-98c4-d350-ce57b7db7bff>
2013-05-08 09:08:07 +0000

    Update Login.py
    git-svn-id: https://github.com/Inconditus/win.git/trunk@101 11aaf2a9-4230-98c4-d350-ce57b7db7bff

--- Login.py
+++ Login.py
@@ -1,19 +1,43 @@
 """User login preprocessing module
 """
 
-import User
 
-
-def inputCreateUser(username, password):
+def inputCreateUser(user):
   """Preprocess new user info
+  
+  Args:
+    dict user: Dict representing all fields of a UserInfo object
+    
+  Return:
+    UserInfo: The newly created user object
   """
 
 
 def inputLogin(username, password):
   """Preprocess login information
+  
+  Args:
+    string username: The potential username
+    string password: The potential password
+    
+  Return:
+    UserInfo: UserInfo object of the logged in user
   """
   
 
-def inputLogout():
+def inputLogout(user):
   """Preprocess logout
+  
+  Args:
+    UserInfo user: UserInfo object of the user being logged out.
+  """
+  
+  
+def inputEditUser(user, attr):
+  """Preprocess updating user profile
+  
+  Args:
+    UserInfo user: The UserInfo object of the user being modified.
+    dict attr: A dict specifying user attributes, of the form {email:*, DOB:*, etc}. Not every
+               field stored in the database has to be present.
   """
